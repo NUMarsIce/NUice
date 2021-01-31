86cb8d8c1ad2f67c9cda22483dd4d69f5b42bb85
Ian <spyguyian@gmail.com>
2021-01-31 11:59:18 -0500

    setup structure for firmware

---
+++ firmware/cat_board/cat_board.ino
@@ -0,0 +1,48 @@
+/**
+ * 
+ * 
+ * 
+ **/
+#include <Arduino.h>
+#include <ros.h>
+#include <std_msgs/Empty.h>
+#include <NU32Hardware.h>
+
+ros::NodeHandle_<NU32Hardware> nh;
+
+////////////////
+// Subscriber 
+////////////////
+void messageCb( const std_msgs::Empty& toggle_msg){
+  digitalWrite(PA_9, HIGH-digitalRead(PA_9));   // blink the led
+}
+ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb );
+
+
+////////////////
+// Publishers
+////////////////
+
+
+////////////////
+// Services
+////////////////
+
+void setup() {
+  //Initialise ros
+  nh.initNode();
+  //Subscribers
+  //Publishers
+  //Services
+  nh.subscribe(sub);
+
+  //Peripheral setup
+  pinMode(PA_9, OUTPUT);
+}
+
+void loop() {
+  //loop
+  nh.spinOnce();
+  delay(1);
+}
+
---
+++ firmware/libraries/internal/README.md
@@ -0,0 +1,1 @@
+Directory for internally created libraries
