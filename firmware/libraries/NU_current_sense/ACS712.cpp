0d6ab594b0030c394ffbe3e8b0f6177f3ae76d6f
Ian <spyguyian@gmail.com>
2021-02-03 22:26:55 -0500

    create current sensor class

--- firmware/libraries/NU_current_sense/ACS712.cpp
+++ firmware/libraries/NU_current_sense/ACS712.cpp
@@ -1,17 +1,47 @@
-/*
- * ACS712 Class implementation
- */
+#include "ACS712.h"
 
-#include "headers/ACS712.h"
 ACS712::ACS712(int pin, int mVperAmp) { 
   analog_pin_ = pin; 
   mVperAmp_ = mVperAmp;
-  }
+}
 
 ACS712::~ACS712(){}
 
 float ACS712::read() {
-  float RawValue = analogRead(analog_pin_);
-  float Voltage = (RawValue / 1024.0) * 5000;  // Gets you mV
-  return ((Voltage - ACS_OFFSET_) / mVperAmp_);
+  float raw_value = analogRead(analog_pin_);
+  float voltage = (raw_value / 1024.0) * 5000;  // Gets you mV
+  return ((voltage - 2500) / mVperAmp_);
+}
+
+float ACS712::readAC() {
+  // Find the mean of squares
+  float sqsum = 0;
+  for (int i = 0; i < 30; i++) {
+    sqsum = sqsum + (pow(rolling_buf_[i], 2));
+  }
+
+  // find the square root of that mean
+  return sqrt(sqsum / 30);
+}
+
+void ACS712::updateAC(){
+  if(millis()-last_sample_time_ < 1/1800)//force the update to happen at ~1800Hz (30*60 aka 30 samples per 60Hz AC period)
+    break;
+
+  float raw_value = analogRead(analog_pin_);
+  float voltage = (raw_value / 1024.0) * 5000;  // Gets you mV
+  float sample_current = ((voltage - 2500) / mVperAmp_);
+
+  // Update the buffer
+  rolling_buf_[buffer_index_] = sample_current;
+
+  // update the roll index
+  if (buffer_index_ >= (29)) {
+    buffer_index_ = 0;
+  } else {
+    buffer_index_++;
+  }
+
+  //update sample time
+  last_sample_time_ = millis()
 }
--- firmware/libraries/NU_current_sense/ACS712.h
+++ firmware/libraries/NU_current_sense/ACS712.h
@@ -3,16 +3,46 @@
 
 #include <Arduino.h>
 
-class ACS712 {
- public:
-  ACS712(int pin, int mVperAmp = 66);
-  ~ACS712();
-  float read();
+/**
+ * Driver for the ACS712 Current sensor. This sensor comes in multiple current ratings
+ * Author: Ian Burwell 2/3/21
+ */
 
- private:
-  int analog_pin_;
-  int ACS_OFFSET_ = 2500;
-  int mVperAmp_;  // use 100 for 20A Module and 66 for 30A Module
+//mV per Amp for different varients
+static const int ACS712_20A = 100;
+static const int ACS712_30A = 66;
+
+class ACS712 {
+  public:
+
+    /**
+     * 
+     */
+    ACS712(int pin, int mVperAmp = ACS712_20A, int );
+    ~ACS712();
+    
+    /**
+     * Read the current current in amps
+     */
+    float read();
+    
+    /**
+     * Read the current AC current in amps
+     */
+    float readAC();
+    
+    /**
+     * Update the RMS calculation for AC current. Must be called at greater than 1.8kHz
+     */
+    void updateAC();
+
+  private:
+    float rolling_buf_[30];
+    int buffer_index_ = 0;
+    unsigned long last_sample_time_ = millis();
+
+    int analog_pin_;
+    int mVperAmp_;  
 };
 
 #endif /* _ACS_712_H_ */
