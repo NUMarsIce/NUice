af8f6d32b2efaf5538f5ab8b2c49a1f4c241a517
Kyle Ednie <ednie.k@husky.neu.edu>
2021-03-03 21:46:50 -0500

    eat shit and die ros

--- firmware/test_drive/test_drive.ino
+++ firmware/test_drive/test_drive.ino
@@ -12,7 +12,8 @@ void tare(const std_srvs::EmptyRequest &req, std_srvs::EmptyResponse &res);
 void set_speed(const std_msgs::Float64 & speed_msg);
 
 ros::NodeHandle_<NU32Hardware> nh;
-std_msgs::Int64 pub_msg, stepper_msg;
+std_msgs::Int64 pub_msg;
+std_msgs::Float64 pub_msg;
 ros::Publisher load_cell_pub("/load_cell", &pub_msg);
 ros::Publisher stepper_pos_pub("/stepper_position", &stepper_msg);
 ros::ServiceServer<std_srvs::EmptyRequest, std_srvs::EmptyResponse> stepper_pos_srv("/tare", &tare);
@@ -28,13 +29,14 @@ void setup() {
     nh.advertiseService<std_srvs::EmptyRequest, std_srvs::EmptyResponse>(stepper_pos_srv);
     accel_stepper = AccelStepper(AccelStepper::FULL2WIRE, PA_2, PA_3);
     load_cell = HX711(PA_6, PA_7);
+    load_cell.set_scale(-6900);
 }
 
 void loop() {
     nh.spinOnce();
     stepper_msg.data = accel_stepper.currentPosition();
     stepper_pos_pub.publish(&stepper_msg);
-    pub_msg.data = load_cell.read();
+    pub_msg.data = load_cell.getUnits();
     load_cell_pub.publish(&pub_msg);
     accel_stepper.runSpeed();
     delay(250);
