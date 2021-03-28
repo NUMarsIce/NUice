64e0cb1b8873dd05fa48f945b6a4d3405969aa69
Kyle Ednie <ednie.k@husky.neu.edu>
2021-03-28 17:55:52 -0400

    Analog Hall Effect

--- firmware/playground/hall_effect_sketch/hall_effect_sketch.ino
+++ firmware/playground/hall_effect_sketch/hall_effect_sketch.ino
@@ -1,25 +1,43 @@
 #include <Arduino.h>
 #include <ros.h>
-#include <std_msgs/Empty.h>
-#include <NU32Hardware.h>
+#include <std_msgs/Float64.h>
+#include <NU_Driver.h>
 
-ros::NodeHandle_<NU32Hardware> nh;
-pin = #
+Class HallEffectDriver: public NUDriver {
+  private:
+  ros::Publisher pub;
+  std_msgs::Float64 msg;
+  int pin;
+  float last_ping;
+  float time_out;
 
-std_msgs::Empty msg;
-ros::Publisher pub("/hall_effect", &msg);
+  HallEffectDriver(int pin, std::string topic_name): NUDriver(ros::NodeHandle_<NU32Hardware>(), "hall_effect/") {
+    pub = ros::Publisher(topic_name, &msg);
+    this->pin = pin;
+  }
 
-void setup() {
-  nh.initNode();
-  nh.advertise(pub);
-  pinMode(pin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(pin), publish, RISING);
-  
-}
-void loop() {
-  nh.spinOnce();
-}
+  void setup() {
+    nh.initNode();
+    nh.advertise(pub);
+    pinMode(pin, INPUT);
+    attachInterrupt(digitalPinToInterrupt(pin), publish, RISING);
+    time_out = 5000;
+    last_ping = millis();
+  }
 
-void publish() {
-  pub.publish(&msg);
-}
+  void update() {
+    nh.spinOnce();
+  }
+
+  void publish() {
+    float ping = millis();
+    float dt = ping - last_ping;
+    if(dt > time_out) {
+      return;
+    } 
+    msg.data = 1.0/dt;
+    pub.publish(&msg);
+    last_ping = ping;
+  }
+
+};
