299d5c4f51e069d5951e751eaa8d0c1d98b43205
Ian <spyguyian@gmail.com>
2021-04-15 20:42:55 -0400

    Fixed dc motor driver

--- firmware/libraries/NU_motor/NU_DCMotor_L298N.h
+++ firmware/libraries/NU_motor/NU_DCMotor_L298N.h
@@ -1,19 +1,21 @@
 #include <Arduino.h>
 #include <NUros.h>
+#include <NU_Driver.h>
 #include <std_msgs/Bool.h>
-#include <std_msgs/Int8.h>
+#include <std_msgs/UInt8.h>
 
-class DCMotorDriver: public NUDriver {
+class DCMotorDriver : NUDriver {
 private:
-    ros::Subscriber<std_msgs::Bool> dir_sub;
-    ros::Subscriber<std_msgs::Int8> speed_sub;
-    std_msgs::Bool direction_msg;
+    ros::Subscriber<std_msgs::Bool, DCMotorDriver> dir_sub;
+    ros::Subscriber<std_msgs::UInt8, DCMotorDriver> speed_sub;
     int dir_pin_1, dir_pin_2, speed_pin;
 
 public:
-    DCMotorDriver(ros::NUNodeHandle& nh, const char* ns, int dir_pin_1, int dir_pin_2, int speed_pin): NUDriver(nh, ns) {
-        dir_sub = ros::Subscriber<std_msgs::Bool>(appendNamespace("/motor_dir"), &set_direction);
-        speed_sub = ros::Subscriber<std_msgs::Int8>(appendNamespace("/motor_speed"), &set_speed);
+    DCMotorDriver(ros::NUNodeHandle& nh, const char* ns, int dir_pin_1, int dir_pin_2, int speed_pin) : 
+                dir_sub(appendNamespace("/motor_dir"), &DCMotorDriver::set_direction, this),
+                speed_sub(appendNamespace("/motor_speed"), &DCMotorDriver::set_speed, this),
+                NUDriver(nh, ns){
+
         this->dir_pin_1 = dir_pin_1;
         this->dir_pin_2 = dir_pin_2;
         this->speed_pin = speed_pin;
@@ -22,8 +24,12 @@ public:
     void setup() {
         pinMode(dir_pin_1, OUTPUT);
         pinMode(dir_pin_2, OUTPUT);
-        nh.subscribe(dir_sub);
-        nh.subscribe(speed_sub);
+
+        digitalWrite(dir_pin_1, HIGH);
+        digitalWrite(dir_pin_2, LOW);
+
+        nh_.subscribe(dir_sub);
+        nh_.subscribe(speed_sub);
     }
 
     void update() {}
@@ -38,7 +44,7 @@ public:
         }
     }
 
-    void set_speed(const std_msgs::Int8 & speed_msg) {
+    void set_speed(const std_msgs::UInt8 & speed_msg) {
         analogWrite(speed_pin, speed_msg.data);
     }
 };
--- firmware/playground/dc_motor_sketch/dc_motor_sketch.ino
+++ firmware/playground/dc_motor_sketch/dc_motor_sketch.ino
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 #include <NUros.h>
-#include <NU_DCMotor.h>
+#include <NU_DCMotor_L298N.h>
 
 ros::NUNodeHandle nh;
-DCMotorDriver dc(nh, "dc_motor", #, #, #);
+DCMotorDriver dc(nh, "dc_motor", PA_5, PA_6, PA_7);
 
 void setup() {
     nh.getHardware()->setBaud(115200);
