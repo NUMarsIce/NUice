d8b54949c550f18f9f1279c686960fc1f1b24205
Kyle Ednie <ednie.k@husky.neu.edu>
2021-04-16 00:03:03 -0400

    Fixed logging

--- catkin_ws/src/nuice_simulations/src/logging_sim/logging_sim_node.py
+++ catkin_ws/src/nuice_simulations/src/logging_sim/logging_sim_node.py
@@ -2,6 +2,7 @@
 
 import rospy
 from std_msgs.msg import Float64
+import os.path
 
 values_list = []
 
@@ -15,13 +16,18 @@ def run():
     #Get the desired name of the data file and the topics to log.
     rospy.init_node("logging_node", anonymous=True)
     topics_list = rospy.get_param("/topics_list")
-    data_file_name = rospy.get_param("/data_file_name")
+    data_file_name = rospy.get_param("/data_file_name", "log")
     values_list = [0.0] * len(topics_list)
     #Subscribe to the topics in the list.
     for i in range(len(topics_list)):
         rospy.Subscriber(topics_list[i], Float64, lambda msg,index=i: callback(msg, index))
     rate = rospy.Rate(2)
-    with open(data_file_name, "w") as data_file:
+    if (os.path.isfile(data_file_name + ".csv")):
+        i = 1
+        while (os.path.isfile(data_file_name + str(i)+ ".csv")):
+            i += 1
+        data_file_name += str(i)
+    with open(data_file_name + ".csv", "w") as data_file:
         start = rospy.Time.now()
         #Write seconds since start of mission, followed by data in order specified by the topics file.
         while not rospy.is_shutdown():
