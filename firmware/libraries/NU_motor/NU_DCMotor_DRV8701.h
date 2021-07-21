eda0f013f61c70d017b4dd296598613fcffb3eb2
Ian <spyguyian@gmail.com>
2021-07-21 18:05:49 -0400

    Working movement board

--- firmware/libraries/NU_motor/NU_DCMotor_DRV8701.h
+++ firmware/libraries/NU_motor/NU_DCMotor_DRV8701.h
@@ -5,29 +5,23 @@
 #include <std_msgs/UInt8.h>
 #include <std_msgs/Float32.h>
 
-
-#define CUR_PIN  PA6
-#define PH_PIN  PB10
-#define EN_PIN  PB13
-#define NSLP_PIN PC7
-#define NFLT_PIN PA8
-
 /**
  * Driver for the DC motor driver on the movement board.
  */
-class DRV8701Driver : public NUDriver {
+class NUDRV8701Driver : public NUDriver {
 
 public:
     /**
      * Constructs the driver based based on what number driver it is on the movement board which has dedicated pins for each driver
      */
-    DRV8701Driver(ros::NUNodeHandle& nh, const char* ns, uint8_t movement_board_number) :
+    NUDRV8701Driver(ros::NUNodeHandle& nh, const char* ns, uint8_t movement_board_number, uint8_t update_hz = 4) :
                 NUDriver(nh, ns),
-                dir_sub(appendNamespace("/set_dir"), &DRV8701Driver::dir_cb, this),
-                speed_sub(appendNamespace("/set_speed"), &DRV8701Driver::speed_cb, this),
-                speed_sub(appendNamespace("/enable"), &DRV8701Driver::enable_cb, this),
+                dir_sub(appendNamespace("/set_dir"), &NUDRV8701Driver::dir_cb, this),
+                speed_sub(appendNamespace("/set_speed"), &NUDRV8701Driver::speed_cb, this),
+                en_sub(appendNamespace("/enable"), &NUDRV8701Driver::enable_cb, this),
                 current_pub(appendNamespace("/current"), &current_pub_msg_),
-                fault_pub(appendNamespace("/fault"), &fault_pub_msg_)
+                fault_pub(appendNamespace("/fault"), &fault_pub_msg_),
+                update_hz_(update_hz)
     {
         switch(movement_board_number){
             case 3: //PC4, PB0, PB1, PC8, PA10
@@ -44,7 +38,7 @@ public:
                 nsleep_pin_ = PC6;
                 nfault_pin_ = PC9;
                 break;
-            default; //case 0   PA6, PB10, PB13, PC7, PA8
+            default: //case 1   PA6, PB10, PB13, PC7, PA8
                 cur_pin_ = PA6;
                 ph_pin_ = PB10;
                 en_pin_ = PB13;
@@ -54,11 +48,11 @@ public:
         }
     }
 
-    DRV8701Driver(ros::NUNodeHandle& nh, const char* ns, uint8_t cur_pin, uint8_t ph_pin, uint8_t en_pin, uint8_t nsleep_pin, uint8_t nfault_pin, uint8_t update_hz = 4) : 
+    NUDRV8701Driver(ros::NUNodeHandle& nh, const char* ns, uint8_t cur_pin, uint8_t ph_pin, uint8_t en_pin, uint8_t nsleep_pin, uint8_t nfault_pin, uint8_t update_hz = 4) : 
                 NUDriver(nh, ns),
-                dir_sub(appendNamespace("/set_dir"), &DRV8701Driver::dir_cb, this),
-                speed_sub(appendNamespace("/set_speed"), &DRV8701Driver::speed_cb, this),
-                speed_sub(appendNamespace("/enable"), &DRV8701Driver::enable_cb, this),
+                dir_sub(appendNamespace("/set_dir"), &NUDRV8701Driver::dir_cb, this),
+                speed_sub(appendNamespace("/set_speed"), &NUDRV8701Driver::speed_cb, this),
+                en_sub(appendNamespace("/enable"), &NUDRV8701Driver::enable_cb, this),
                 current_pub(appendNamespace("/current"), &current_pub_msg_),
                 fault_pub(appendNamespace("/fault"), &fault_pub_msg_),
                 cur_pin_(cur_pin), ph_pin_(ph_pin), en_pin_(en_pin), nsleep_pin_(nsleep_pin), nfault_pin_(nfault_pin)
@@ -73,19 +67,21 @@ public:
 
         nh_.subscribe(dir_sub);
         nh_.subscribe(speed_sub);
+        nh_.subscribe(en_sub);
 
         nh_.advertise(current_pub);
         nh_.advertise(fault_pub);
     }
 
     void update() {
-        if(millis()-last_update_ > update_hz_){
+
+        if(millis()-last_update_ > 1000.0f/update_hz_){
             current_pub_msg_.data = (float)analogRead(cur_pin_)/1023*3.3/20/0.01; // Current reads 20x the voltage across the 0.01ohm resistor. 
-            current_pub.publish(current_pub_msg_);
+            current_pub.publish(&current_pub_msg_);
 
             fault_pub_msg_.data = digitalRead(nfault_pin_);
-            fault_pub.publish(fault_pub_msg_);
-
+            fault_pub.publish(&fault_pub_msg_);
+            
             last_update_ = millis();
         }
     }
@@ -97,17 +93,17 @@ private:
     uint8_t update_hz_;
 
     //Subscribers
-    ros::Subscriber<std_msgs::Bool, DRV8701Driver> dir_sub;
+    ros::Subscriber<std_msgs::Bool, NUDRV8701Driver> dir_sub;
     void dir_cb(const std_msgs::Bool& msg){
         digitalWrite(ph_pin_, msg.data);
     }
 
-    ros::Subscriber<std_msgs::Bool, DRV8701Driver> en_sub;
+    ros::Subscriber<std_msgs::Bool, NUDRV8701Driver> en_sub;
     void enable_cb(const std_msgs::Bool& msg){
-        digitalWrite(en_pin_, msg.data);
+        digitalWrite(nsleep_pin_, msg.data);
     }
 
-    ros::Subscriber<std_msgs::UInt8, DRV8701Driver> speed_sub;
+    ros::Subscriber<std_msgs::UInt8, NUDRV8701Driver> speed_sub;
     void speed_cb(const std_msgs::UInt8& msg){
         analogWrite(en_pin_, msg.data);
     }
--- firmware/parsec/movement_board/movement_board.ino
+++ firmware/parsec/movement_board/movement_board.ino
@@ -1,20 +1,33 @@
 
 #include <Arduino.h>
-#include <ros.h>
+#include <NUros.h>
 #include <NU_GPIO.h>
+#include <NU_DCMotor_DRV8701.h>
+#include <NU_QuadratureEncoder.h>
 
-ros::NodeHandle nh;
 
-NUGPIO led(nh, "led", PA9);
+ros::NUNodeHandle nh;
+
+NUGPIO led(nh, "led", PA9, OUTPUT);
+NUDRV8701Driver carosel_mot(nh, "carosel", 2);//M2
+NUDRV8701Driver brake1(nh, "brake1", 3);//M3
+// NUDRV8701Driver brake2(nh, "brake2", 1);//M1
+
+// NUGPIO led(nh, "carosel_lim", PA10, OUTPUT);
+// NUQuadratureEncoder carosel_enc(nh, "carosel", a, b);
 
 void setup(){
     nh.getHardware()->setBaud(115200);
     nh.initNode();
     led.setup();
+    carosel_mot.setup();
+    brake1.setup();
 }
 
 
 void loop(){
     nh.spinOnce();
     led.update();
+    carosel_mot.update();
+    brake1.update();
 }
