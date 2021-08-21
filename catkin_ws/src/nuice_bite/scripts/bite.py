b60ca66e40478915b88c1bfa1a3fc0f85743cdd8
JJEW22 <john.e.wilkins22@gmail.com>
2021-08-20 22:47:42 -0400

    created initial implementation of BITE with simple logistic regression model to be used for testing

--- catkin_ws/src/nuice_bite/scripts/bite.py
+++ catkin_ws/src/nuice_bite/scripts/bite.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
+import os
 import rospy
 import pickle
 from std_msgs.msg import String
 from std_msgs.msg import Float32
 from std_msgs.msg import Float64
 from std_msgs.msg import Int32
+from ProbabilityVector.msg import ProbabilityVector
 
 POSITION_STRING = 'position'
 WOB_STRING = 'wob'
@@ -22,8 +24,8 @@ feature_vector = {
 
 def load_model():
     # TODO: get a correct filename
-    filename = 'bite_model'
-    infile = open(filename,'rb')
+    filepath = os.path.join(os.getcwd(), 'catkin_ws', 'src', 'nuice_bite', 'logistic_model.sav')
+    infile = open(filepath,'rb')
     model = pickle.load(infile)
     infile.close() 
     return model
@@ -38,27 +40,52 @@ def read_drill_hall(data):
     feature_vector[SPIN_SPEED_STRING] = data.data
 
 def read_current(data):
-    feature_vector[CURRENT_STRING] = data.
+    feature_vector[CURRENT_STRING] = data.data
+
+def probability_list_to_probability_vector(probability_list, states):
+    probability_vector = []
+    for state_index in range(len(states)):
+        layer_status = LayerStatus()
+        layer_status.layer_name = states[state_index]
+        layer_status.propability = probability_list[state_index]
+        probability_vector.append(layer_status)
+    
+def feature_dict_to_vector():
+    # TODO: update this to work for values other than wob
+    return [feature_vector[WOB_STRING]]
+
 
 def bite():
     # load BITE from pickle
     model = load_model()
+    classes = model.classes_
 
+    # pub = rospy.Publisher('bite_probabilities', ProbabilityVector, queue_size=10)
     pub = rospy.Publisher('bite_probabilities', String, queue_size=10)
     rospy.init_node('bite', anonymous=True)
 
     # step up feature vector subsciping information
     rospy.Subscriber("drill_loadcell/load", Float32, read_wob)
-    rospy.Subscriber("drill_hall/rate", Float64, read_drill_hall)
-    rospy.Subscriber("drill_stp/current_position", Int32, read_position)
-    rospy.Subscriber("drill_current", Float32, read_wob)
+
+    # currently commented out because initial model used for testing only has wob data
+    # rospy.Subscriber("drill_hall/rate", Float64, read_drill_hall)
+    # rospy.Subscriber("drill_stp/current_position", Int32, read_position)
+    # rospy.Subscriber("drill_current", Float32, read_wob)
 
 
     rate = rospy.Rate(1) # 1hz
     while not rospy.is_shutdown():
-        # TODO: properly format predictions output
-        predictions = model.predict(feature_vector)
-        pub.publish(predictions)
+        feature_list = feature_vector_to_list(feature_vector)
+        # current model requires a list of feature vectors
+        probability_list = model.predict_proba([feature_vector])[0]
+        prediction = model.predict(feature_vector)[0]
+
+        # WILL BE USED FOR CUSTOM MESSAGING
+        # probability_vector = probability_list_to_probability_vector(probability_list, states)
+        # msg = ProbabilityVector()
+        # msg.probability_vector = probability_vector
+
+        pub.publish(prediction)
         rate.sleep()
 
 if __name__ == '__main__':
