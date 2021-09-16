92bddb65a923b807943abf671f8c4c26b655baa3
NUMARSICE <numarsice@outlook.com>
2021-09-16 13:04:59 -0400

    driver_grounddetection fix

--- catkin_ws/src/nuice_abstraction/nodes/driver_ground_detection.py
+++ catkin_ws/src/nuice_abstraction/nodes/driver_ground_detection.py
@@ -5,9 +5,9 @@ from std_msgs.msg import Int32, UInt16, Float32
 import sys
 
 NUDGE_VAL = -50
-SPEED = 100
-LOAD_DELTA = 25
-STEPS_PER_METER = 118110
+SPEED = 200
+LOAD_DELTA = 10
+STEPS_PER_METER = -118110
 
 load = 0
 pos = 0
@@ -27,27 +27,32 @@ def main():
     speed_pub = rospy.Publisher("/central_board/drill_stp/set_max_speed", UInt16, queue_size=10)
 
     rospy.Subscriber("/central_board/drill_stp/current_position", Int32, pos_cb)
-    rospy.Subscriber("/drill_board/drill_loadcell/load", Float32, load1_cb)
+    rospy.Subscriber("/drill_board/drill_loadcell/load", Float32, load_cb)
 
     speed_pub.publish(UInt16(SPEED))
 
     ### Sketchy code start 
     print("Waiting for load data...")
-    load_base = rospy.wait_for_message("/drill_board/drill_loadcell/load", Float32) 
+    load_base = rospy.wait_for_message("/drill_board/drill_loadcell/load", Float32).data
     print("Beginning ground detection\n")
+
+    # move a bit before resetting load_base
+    rel_pos_pub.publish(Int32(-1000))
+    rospy.sleep(1)
+    load_base = load
 
     r = rospy.Rate(10) #10 Hz
     while not rospy.is_shutdown() and abs(load_base-load) < LOAD_DELTA:
         r.sleep()
         # move down
         rel_pos_pub.publish(Int32(NUDGE_VAL))
-        print 'Pressure: %.2f | Position: %.6f\r' % (load, pos/STEPS_PER_METER),
+        print 'dPressure: %.2f | Position: %.4f\r' % (abs(load_base-load), float(pos)/STEPS_PER_METER),
         sys.stdout.flush()
 
     # stop
     rel_pos_pub.publish(Int32(0))
     
-    print("\nEnd position is %.6fm (%i)" % (pos/STEPS_PER_METER, pos))
+    print("\nEnd position is %.6fm (%i)" % (float(pos)/STEPS_PER_METER, pos))
 
 
 if __name__ == '__main__':
