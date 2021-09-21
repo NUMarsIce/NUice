c81e34271395df0d890ca6906dfe2a531db8751c
JJEW22 <john.e.wilkins22@gmail.com>
2021-09-20 22:21:06 -0400

    changed scale factor

--- catkin_ws/src/nuice_bite/scripts/bite.py
+++ catkin_ws/src/nuice_bite/scripts/bite.py
@@ -33,7 +33,7 @@ def load_model():
     return model
 
 def read_position(data):
-    feature_vector[POSITION_STRING] = -1016.0/(300.0 * 400.0) * data.data
+    feature_vector[POSITION_STRING] = -3.26571432/(400.0) * data.data
 
 def read_wob(data):
     feature_vector[WOB_STRING] = data.data
